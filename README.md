24ecc54c8147c849a6fd1fb20fe4d2ef470a1568
chaveena <42973876+chaveena@users.noreply.github.com>
2024-03-20 13:48:34 -0700

    oops wrong folder

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-first run:
+first run in the sagemaker terminal:
 pip install --no-cache-dir -r requirements.txt
+OR
+sh setup.sh
 
-to run it, go to terminal and type:
-sh run.sh summary_from_yt.py
-or 
+to run the app, go to terminal and type:
 sh run.sh local_upload.py
+- this is a shellscript that creates a proxy to allow you to host a streamlit app from sagemaker. if you try to just use the streamlit command it won't work, the two IPs that are generated will not be accessible
+
+summary_from_yt.py works, but it doesn't have the newer functionality and it requires the stupid GCP API key
+
+video_to_frames_decord is from github, i didn't write it. and basically it just pulls frames out of a file, sampling how you want, and stores them in a directory (in our case, frames)
+
+run sh cleanup.sh when you're done, to shut down whatever streamlit stuff is running
 
 much of this from: https://aws.amazon.com/blogs/machine-learning/build-streamlit-apps-in-amazon-sagemaker-studio/
 
--- local_upload.py
+++ local_upload.py
@@ -11,7 +11,7 @@ from decord import VideoReader
 from decord import cpu, gpu
 from video_to_frames_decord import extract_frames, video_to_frames
 import base64
-
+import collections
 
 
 def transcribe_file(audio_file_name, transcribe_client):
@@ -31,7 +31,7 @@ def transcribe_file(audio_file_name, transcribe_client):
           MediaFormat = file_format,
           LanguageCode='en-US'
         )
-    
+    # wait until the Transcribe job finishes
     max_tries = 60
     while max_tries > 0:
         max_tries -= 1
@@ -47,7 +47,8 @@ def transcribe_file(audio_file_name, transcribe_client):
             print(f"Waiting for {job_name}. Current status is {job_status}.")
         time.sleep(10)    
 
-# write a function to get a list of current Transcribe jobs, and then to check if an amazon Transcribe job already exists with the full name of the file in S3
+        
+# write a function to get a list of current Transcribe jobs, and then to check if an amazon Transcribe job already exists for this file
 def check_job_name(job_name, transcribe_client):
     current_jobs = transcribe_client.list_transcription_jobs()
     for job in current_jobs['TranscriptionJobSummaries']:
@@ -56,9 +57,8 @@ def check_job_name(job_name, transcribe_client):
     return job_name
 
 
-        
-def get_summmary(transcript):
-    brt = boto3.client(service_name='bedrock-runtime')
+# not used anymore       
+def get_transcript_summmary(transcript, bedrock_client):
     prompt = '"\n\nHuman: Summarize in 2-3 lines: "' + transcript + '\n\nAssistant:"'
     body = json.dumps({
         "prompt": prompt,
@@ -77,23 +77,44 @@ def get_summmary(transcript):
     return(summary)
 
 
-def upload_to_s3(uploaded_file, bucket, s3_path):
+# get the full summary based on both audio and visuals
+def get_AV_summmary(transcript, frame_descriptions, bedrock_client):
+    prompt = '"\n\nHuman: Your goal is to give me a comprehensive understanding of a video, using a transcript of the audio and descriptions of the frames. Give me a summary of the video, and tell me what the purpose of the video is. This should be no more than 5 sentences. You can include descriptions of individual frames and excerpts from the transcript to make your case. This is the transcript: "' + transcript + "\n and this is the descriptions of the frames, displayed in a dictionary: " + str(frame_descriptions) + '\n\nAssistant:"'
+    body = json.dumps({
+        "prompt": prompt,
+        "max_tokens_to_sample": 300,
+        "temperature": 0.1,
+        "top_p": 0.9,
+    })
+    modelId = 'anthropic.claude-v2'
+    accept = 'application/json'
+    contentType = 'application/json'
+    response = bedrock_client.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response_body = json.loads(response.get('body').read())
+    # text
+    summary = response_body.get('completion')
+    print(summary)
+    return(summary)
+
+
+def upload_to_s3(uploaded_file, bucket, s3_path, s3_client):
     if uploaded_file is not None:
-        s3 = boto3.client('s3')
         filename = uploaded_file.name
-        s3.upload_fileobj(uploaded_file, bucket, filename)
-        #wait until s3.upload_fileobj job has completed and the file has fully uploaded
+        s3_client.upload_fileobj(uploaded_file, bucket, filename)
+        # wait until s3.upload_fileobj job has completed and the file has fully uploaded
         while True:
             try:
-                s3.head_object(Bucket=bucket, Key=filename)
-                st.write(filename, "successfully uploaded to S3")
+                s3_client.head_object(Bucket=bucket, Key=filename)
+                st.write(filename, "Successfully uploaded to S3")
                 break
             except:
                 time.sleep(1)
         s3_path = "s3://" + bucket + "/" + filename
     return s3_path
 
-def call_haiku(base64_string, bedrock_client):
+
+# this makes individual calls to claude for each extracted frame
+def get_frame_descriptions(base64_string, bedrock_client):
     prompt_config = {
         "anthropic_version": "bedrock-2023-05-31",
         "max_tokens": 4096,
@@ -109,14 +130,14 @@ def call_haiku(base64_string, bedrock_client):
                             "data": frame_base64_str,
                         },
                     },
-                    {"type": "text", "text": "Provide a caption for this image in one sentence"},
+                    {"type": "text", "text": "Provide a caption for this image in one sentence. Use clear, straightforward language, without embellishing with subjective adjectives."},
                 ],
             }
         ],
     }
 
     body = json.dumps(prompt_config)
-
+    
     modelId = "anthropic.claude-3-haiku-20240307-v1:0"
     accept = "application/json"
     contentType = "application/json"
@@ -125,44 +146,57 @@ def call_haiku(base64_string, bedrock_client):
         body=body, modelId=modelId, accept=accept, contentType=contentType
     )
     response_body = json.loads(response.get("body").read())
-
     results = response_body.get("content")[0].get("text")
     return results
 
-'''
-st.title('video summarizer') 
-st.write('For uploaded videos') 
-'''
-uploaded_file = False
-uploaded_file = st.file_uploader("Choose a file")
-s3_path = ""
-bucket = "dundundundundundun"
 
+# initializing variables
+s3_path = "" # will define later
+bucket = "dundundundundundun" # s3 bucket name to store the video, for transcribe to pull from
+frames_dir = "frames" # name of the top level diectory to store frames
+uploaded_file = False # will become true when a file is uploaded
+
+# create the AWS clients
+bedrock_client = boto3.client(service_name='bedrock-runtime')
+transcribe_client = boto3.client("transcribe")
+s3_client = boto3.client('s3')
+
+st.title('Video Summarizer') 
+st.write('For uploaded videos only, use the other file for youtube URLs') 
+uploaded_file = st.file_uploader("Choose a file")
 submit_button = st.button("Submit", type="primary")
+
 if submit_button and uploaded_file:
     filename = uploaded_file.name
-    video_to_frames(filename, uploaded_file, frames_dir='frames', overwrite=False, every=100)
-    # create a bedrock runtime client
-    bedrock_client = boto3.client(service_name='bedrock-runtime')
-    for frame in os.listdir('frames/animal-short-clip.mp4'):
+    
+    # we're sampling every 100 frames, hence every = 100
+    video_to_frames(filename, uploaded_file, frames_dir, overwrite=False, every=100)
+       
+    uploaded_file.seek(0)
+    s3_path = upload_to_s3(uploaded_file, bucket, s3_path, s3_client)
+    full_transcript = transcribe_file(s3_path, transcribe_client)
+    
+    results_dict ={}
+    for frame in os.listdir(frames_dir + "/" + filename):
         if frame == ".ipynb_checkpoints":
             continue
-        frame_path = 'frames/animal-short-clip.mp4/' + frame
+        frame_path = frames_dir + "/" + filename + "/" + frame
         # read the frame as a numpy array
-        st.write(frame_path)
         frame_array = cv2.imread(frame_path)
         # convert the frame to a base64-encoded string
         frame_base64 = cv2.imencode('.jpg', frame_array)[1].tobytes()
         # create a base64-encoded string from the frame
-        frame_base64_str = base64.b64encode(frame_base64).decode('utf-8')
-        results = call_haiku(frame_base64_str, bedrock_client)
-        st.write(results)
-    s3_path = upload_to_s3(uploaded_file, bucket, s3_path)
-    transcribe_client = boto3.client("transcribe")
-    full_transcript = transcribe_file(s3_path, transcribe_client)
-    print("Full transcript:", full_transcript)
-    #st.write same as above print statement
-    st.write("Full transcript:", full_transcript)
-    summary = get_summmary(full_transcript)
-    #st.write same as above print statement
+        frame_base64_str = base64.b64encode(frame_base64).decode('utf-8')  
+        # call bedrock
+        results = get_frame_descriptions(frame_base64_str, bedrock_client)
+        results_dict[frame] = results
+        
+    #get the dictionary in order, so that we're able to see the frame descriptions in the order they appear 
+    sorted_results_dict = collections.OrderedDict(sorted(results_dict.items()))
+    summary = get_AV_summmary(full_transcript, sorted_results_dict, bedrock_client)
+
+    st.write("These are explanations of some of the frames in the video, thanks to Claude v3 Haiku:\n")
+    st.write(sorted_results_dict)
+    st.write("Full transcript from Amazon Transcribe:\n", full_transcript)
+    st.write("This is an explanation of the video written by Claude v2, given the transcript and the scene descriptions:\n")
     st.write(summary)
